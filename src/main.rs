b63dc621d9c15c111d223451736da95929b4fa9a
charust <617152237@qq.com>
2022-01-20 20:22:31 +0800

    for dip 721.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use config::AppConfig;
 use ic_agent::{
     agent::http_transport::ReqwestHttpReplicaV2Transport, ic_types::Principal,
-    identity::BasicIdentity, Agent,
+    identity::Secp256k1Identity, Agent,
 };
-use ic_cdk::export::candid::{CandidType, Decode, Encode};
+use ic_cdk::export::candid::{Decode, Encode};
 use log::{error, info};
 use once_cell::sync::Lazy;
-use serde::Deserialize;
 use sqlx::mysql::{MySql, MySqlPoolOptions};
 use sqlx::Error;
 use sqlx::Pool;
+use types::{
+    MetadataDesc, MetadataKeyVal, MetadataPart, MetadataPurpose, MetadataVal, MintReceipt,
+};
 
 static DB: Lazy<Pool<MySql>> = Lazy::new(|| init_db_pool().unwrap());
 static CFG: Lazy<AppConfig> = Lazy::new(AppConfig::new);
@@ -21,66 +23,82 @@ pub fn init_db_pool() -> Result<Pool<MySql>, Error> {
         .connect_lazy(&CFG.database_url)
 }
 
-fn create_identity() -> BasicIdentity {
-    BasicIdentity::from_pem_file(&CFG.icp_config.identity_pem_path)
+fn create_identity() -> Secp256k1Identity {
+    Secp256k1Identity::from_pem_file(&CFG.icp_config.identity_pem_path)
         .expect("Could not read the key pair.")
 }
 
-pub type ActorResult<T> = Result<T, ErrorInfo>;
-
-#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, CandidType, Deserialize)]
-pub struct ErrorInfo {
-    code: u32,
-    message: String,
-}
-
 async fn mint_token(nft_vec: Vec<Nft>) {
+
     let transport = ReqwestHttpReplicaV2Transport::create(&CFG.icp_config.icp_domain).unwrap();
+
     let waiter = garcon::Delay::builder()
         .throttle(std::time::Duration::from_millis(500))
         .timeout(std::time::Duration::from_secs(60 * 5))
         .build();
+
     let agent = Agent::builder()
         .with_transport(transport)
         .with_identity(create_identity())
         .build()
         .unwrap();
+
     //main net need to remove agent.fetch_root_key*()
     let _ = agent.fetch_root_key().await;
+
     let canister_id = Principal::from_text(&CFG.icp_config.agent_canister_id).unwrap();
 
     // mutil mint token
     for nft in nft_vec {
+        let metadata_desc: MetadataDesc = vec![MetadataPart {
+            purpose: MetadataPurpose::Rendered,
+            key_val_data: vec![
+                MetadataKeyVal {
+                    key: "alias".into(),
+                    val: MetadataVal::TextContent(nft.nft_name.clone().unwrap().clone()),
+                },
+                MetadataKeyVal {
+                    key: "bind_status".into(),
+                    val: MetadataVal::Nat8Content(0),
+                },
+            ],
+            data: vec![],
+        }];
+
         let response = agent
-            .update(&canister_id, "mint_token")
+            .update(&canister_id, "mintDip721")
             .with_arg(
                 Encode!(
-                    &nft.nft_name.as_ref().unwrap(),
-                    &Principal::from_text(&nft.p_id.as_ref().unwrap()).unwrap()
+                    &Principal::from_text(&nft.p_id.as_ref().unwrap()).unwrap(),
+                    &metadata_desc
                 )
                 .unwrap(),
             )
             .call_and_wait(waiter.clone())
             .await;
-        if response.is_ok() {
-            let decode_result = Decode!(response.unwrap().as_slice(), ActorResult<bool>).unwrap();
 
-            if let Ok(result) = decode_result {
-                if result {
+        if response.is_ok() {
+
+            let decode_result = Decode!(response.unwrap().as_slice(), MintReceipt).unwrap();
+
+            match decode_result {
+                Ok(mint_receipt_part) => {
                     info!(
-                        "mint :{:?}------response:{:?}\n\n",
+                        "mint :{:?}------response:{:?}\n",
                         &nft.nft_name.as_ref().unwrap(),
-                        decode_result
+                        mint_receipt_part
                     );
-                    // update status = 1
+
+                    //execute sql to update database status of nft to 1. 
                     mint_nft_success_and_update(nft.clone()).await;
                 }
-            } else {
-                error!(
-                    "mint :{:?}------response:{:?}\n\n",
-                    &nft.nft_name.as_ref().unwrap(),
-                    decode_result
-                );
+                Err(api_error) => {
+                    error!(
+                        "mint :{:?}------response:{:?}\n",
+                        &nft.nft_name.as_ref().unwrap(),
+                        api_error
+                    );
+                }
             }
         } else {
             let error = response.unwrap_err();
@@ -121,16 +139,21 @@ pub async fn mint_nft_success_and_update(nft: Nft) {
 
 mod config;
 mod logger;
+mod types;
 
 #[tokio::main]
 async fn main() {
     //log
     logger::start();
+
     info!("*********************************************start********************************************************\n");
+
     //query nft
     let row = todo_nft().await;
+
     //mint token
     //update status
     mint_token(row).await;
+
     info!("*********************************************end**********************************************************\n");
 }
---
+++ src/types.rs
@@ -0,0 +1,55 @@
+use ic_cdk::export::candid::{CandidType, Nat};
+use serde::{Deserialize, Serialize};
+
+pub type MintReceipt = Result<MintReceiptPart, ApiError>;
+
+#[derive(CandidType, Debug, Deserialize)]
+pub enum ApiError {
+    Unauthorized,
+    InvalidTokenId,
+    ZeroAddress,
+    Other,
+    AliasFormatFail(String),
+    AliasHasBeenTaken,
+}
+
+#[derive(CandidType, Deserialize,Debug)]
+pub struct MintReceiptPart {
+    pub token_id: u64,
+    pub id: Nat,
+}
+
+
+pub type MetadataDesc = Vec<MetadataPart>;
+
+#[derive(CandidType, Clone, Deserialize)]
+pub struct MetadataPart {
+    pub purpose: MetadataPurpose,
+    pub key_val_data: Vec<MetadataKeyVal>,
+    pub data: Vec<u8>,
+}
+
+
+#[derive(CandidType, Clone, Deserialize, Serialize)]
+pub enum MetadataPurpose {
+    Preview,
+    Rendered,
+}
+
+#[derive(CandidType, Clone, Deserialize)]
+pub struct MetadataKeyVal {
+    pub key: String,
+    pub val: MetadataVal,
+}
+
+
+#[derive(CandidType, Clone, Deserialize, PartialEq)]
+pub enum MetadataVal {
+    TextContent(String),
+    BlobContent(Vec<u8>),
+    NatContent(Nat),
+    Nat8Content(u8),
+    Nat16Content(u16),
+    Nat32Content(u32),
+    Nat64Content(u64),
+}
